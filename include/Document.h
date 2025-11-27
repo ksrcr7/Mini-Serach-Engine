28e8a231d7bdf4dc63f2e193b8e0b0d5ec25e886
ksrcr7 <kasrabakhshaei25@gmail.com>
2025-11-27 17:02:39 +0330

    Make Class Document that includes path,id and content

---
+++ include/Document.h
@@ -0,0 +1,41 @@
+#ifndef MINI_SEARCH_ENGINE_DOCUMENT_H
+#define MINI_SEARCH_ENGINE_DOCUMENT_H
+
+#pragma once
+#include <string>
+
+class Document{
+public:
+    std::string id;
+    std::string path;
+    std::string content;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif //MINI_SEARCH_ENGINE_DOCUMENT_H
